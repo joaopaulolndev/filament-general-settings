a80673c7c7d75f3a697455aa6a916d43de903ff8
Jefferson Gonçalves <gerson.simao.92@gmail.com>
2025-08-07 21:02:53 -0300

    refactor(provider): remove unused asset and command registrations
    remove unused methods and redundant asset, command, icon, and script data registrations in `FilamentGeneralSettingsServiceProvider`. clean up the class for better readability and maintenance.

--- src/FilamentGeneralSettingsServiceProvider.php
+++ src/FilamentGeneralSettingsServiceProvider.php
@@ -30,7 +30,6 @@ class FilamentGeneralSettingsServiceProvider extends PackageServiceProvider
          * More info: https://github.com/spatie/laravel-package-tools
          */
         $package->name(static::$name)
-            ->hasCommands($this->getCommands())
             ->hasInstallCommand(function (InstallCommand $command) {
                 $command
                     ->publishConfigFile()
@@ -72,76 +71,8 @@ class FilamentGeneralSettingsServiceProvider extends PackageServiceProvider
         //            $this->getAssetPackageName()
         //        );
 
-        FilamentAsset::registerScriptData(
-            $this->getScriptData(),
-            $this->getAssetPackageName()
-        );
-
-        // Icon Registration
-        FilamentIcon::register($this->getIcons());
-
-        // Handle Stubs
-        if (app()->runningInConsole()) {
-            foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
-                $this->publishes([
-                    $file->getRealPath() => base_path("stubs/filament-general-settings/{$file->getFilename()}"),
-                ], 'filament-general-settings-stubs');
-            }
-        }
-
         // Testing
         Testable::mixin(new TestsFilamentGeneralSettings);
-    }
-
-    protected function getAssetPackageName(): ?string
-    {
-        return 'joaopaulolndev/filament-general-settings';
-    }
-
-    /**
-     * @return array<Asset>
-     */
-    protected function getAssets(): array
-    {
-        return [
-            // AlpineComponent::make('filament-general-settings', __DIR__ . '/../resources/dist/components/filament-general-settings.js'),
-            Css::make('filament-general-settings-styles', __DIR__ . '/../resources/dist/filament-general-settings.css'),
-            Js::make('filament-general-settings-scripts', __DIR__ . '/../resources/dist/filament-general-settings.js'),
-        ];
-    }
-
-    /**
-     * @return array<class-string>
-     */
-    protected function getCommands(): array
-    {
-        return [
-            FilamentGeneralSettingsCommand::class,
-        ];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getIcons(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string>
-     */
-    protected function getRoutes(): array
-    {
-        return [];
-    }
-
-    /**
-     * @return array<string, mixed>
-     */
-    protected function getScriptData(): array
-    {
-        return [];
     }
 
     /**
